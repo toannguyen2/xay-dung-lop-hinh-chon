74c56361f1f4a524716fc16fd73dd610f35c6bde
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-04-15 09:50:05 +0700

    add canvas

--- Cricle.js
+++ Cricle.js
@@ -10,6 +10,15 @@ let Cricle = function(radius, color) {
 Cricle.prototype.getRadius = function() {
     return this.radius;
 }
+
 Cricle.prototype.getArea = function() {
     return Math.PI*Math.pow(this.radius, 2);
+}
+
+Cricle.prototype.draw = function(ctx, canvas) {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.beginPath();
+    ctx.arc(this.radius, this.radius, this.radius, 0, 2*Math.PI, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
 }
